3a47dea0dc48d4bdd79f1ab4797e8b31af6b36a6
KIMDONGBEEN <dd1761@naver.com>
2023-02-23 12:11:56 +0900

    23-02-23

--- testServlet/src/main/java/com/hello/HelloServlet.java
+++ testServlet/src/main/java/com/hello/HelloServlet.java
@@ -11,8 +11,15 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+	public void init() {
+		
+	}
    
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+	public void destroy() {
 		
 	}
 
