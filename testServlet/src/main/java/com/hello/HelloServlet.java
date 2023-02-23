7c7d290631c5cdb35e4dbae1e4ac8cb824a51c01
KIMDONGBEEN <dd1761@naver.com>
2023-02-23 14:48:47 +0900

    23-02-23

--- testServlet/src/main/java/com/hello/HelloServlet.java
+++ testServlet/src/main/java/com/hello/HelloServlet.java
@@ -1,26 +1,47 @@
 package com.hello;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+@WebServlet("/HelloServlet")
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	public void init() {
-		
+		System.out.println("init()");
 	}
    
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet()");
 		
+		response.setContentType("text/html");
+		
+		
+//		1.
+//		System.out.println("<html>"); // 콘솔창에 html 출력
+//		
+//		2.
+//		PrintWriter out = new PrintWriter(new FileWriter("result.txt"));	// 파일로 가서 찍히게 됨.
+//		out.println("<html>");
+//		
+//		3.
+		PrintWriter out = response.getWriter(); // 아웃객체를 response로 생성하여 브라우저(web)으로 감.
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Hello Servlet");
+		out.println("</body>");
+		out.println("</html>");
 	}
 	
 	public void destroy() {
-		
+		System.out.println("destory()");
 	}
 
 }
