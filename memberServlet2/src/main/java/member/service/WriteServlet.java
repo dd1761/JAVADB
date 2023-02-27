5850c64d81867a266d8930c650fa7cee3b78adc6
KIMDONGBEEN <dd1761@naver.com>
2023-02-27 15:41:53 +0900

    23-02-27

--- memberServlet2/src/main/java/member/service/WriteServlet.java
+++ memberServlet2/src/main/java/member/service/WriteServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import member.dao.MemberDAO;
+
  
 //@WebServlet("/WriteServlet")
 public class WriteServlet extends HttpServlet {
@@ -24,17 +26,18 @@ public class WriteServlet extends HttpServlet {
 		String gender = request.getParameter("gender");
 		String email1 = request.getParameter("email1");
 		String email2 = request.getParameter("email2");
-		String phone1 = request.getParameter("phone1");
-		String phone2 = request.getParameter("phone2");
-		String phone3 = request.getParameter("phone3");
+		String phone1 = request.getParameter("tel1");
+		String phone2 = request.getParameter("tel2");
+		String phone3 = request.getParameter("tel3");
+		String zipcode = request.getParameter("zipcode");
 		String address1 = request.getParameter("addr1");
 		String address2 = request.getParameter("addr2");
 		String address3 = request.getParameter("addr3");
 	
 		
 		//DB
-		
-		//응
+		MemberDAO memberDAO = new MemberDAO();
+		memberDAO.memberWrite();
 	}
-
+		//응답
 }
