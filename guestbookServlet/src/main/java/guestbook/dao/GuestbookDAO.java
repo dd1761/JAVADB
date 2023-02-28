9fd927535b6a010b294a2197fd4dd40928bdcdad
KIMDONGBEEN <dd1761@naver.com>
2023-02-28 15:57:34 +0900

    23-02-28

--- guestbookServlet/src/main/java/guestbook/dao/GuestbookDAO.java
+++ guestbookServlet/src/main/java/guestbook/dao/GuestbookDAO.java
@@ -12,19 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 public class GuestbookDAO extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    
-    public GuestbookDAO() {
-        super();
-        
-    }
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		doGet(request, response);
