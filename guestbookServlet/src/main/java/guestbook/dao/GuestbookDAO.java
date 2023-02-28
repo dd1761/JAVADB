241dcae04a2b1d75c67545e67de6a2b71f1c4c15
KIMDONGBEEN <dd1761@naver.com>
2023-02-28 17:10:19 +0900

    23-02-28

--- guestbookServlet/src/main/java/guestbook/dao/GuestbookDAO.java
+++ guestbookServlet/src/main/java/guestbook/dao/GuestbookDAO.java
@@ -1,21 +1,71 @@
 package guestbook.dao;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import guestbook.bean.GuestbookDTO;
 
-@WebServlet("/GuestBookDAO")
-public class GuestbookDAO extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+public class GuestbookDAO {
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	private String driver = "oracle.jdbc.driver.OracleDriver";
+	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	private String username = "c##java";
+	private String password = "1234";
+	
+	private static GuestbookDAO guestbookDAO = new GuestbookDAO();
+	
+	public static GuestbookDAO getInstance() {
 		
-		doGet(request, response);
+		return guestbookDAO;
+	}
+	public GuestbookDAO() {
+		try {
+			Class.forName(driver); // Class타입으로 생성
+			System.out.println("driver loading 성공");
+		} catch (ClassNotFoundException e) {
+
+			e.printStackTrace();
+		}
+		// 접속은 한번만 하는것이 아니기 때문에 생성자에서 하면 안됨.
 	}
 
+	public void getConnection() {
+		try {
+			conn = DriverManager.getConnection(url, username, password);/* 오라클 드라이버 */
+			System.out.println("connection 성공");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int guestbook(GuestbookDTO guestbookDTO) {
+		 int su = 0;
+		 
+		 this.getConnection();
+		 String sql = "insert into guestbook values(?,?,?,?,?,sysdate)";
+		 
+		try {
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, guestbookDTO.getName());
+			pstmt.setString(2, guestbookDTO.getEmail());
+			pstmt.setString(3, guestbookDTO.getHomepage());
+			pstmt.setString(4, guestbookDTO.getSubject());
+			pstmt.setString(5, guestbookDTO.getContent());
+			
+			su = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		 
+		 
+		 return su;
+	}
 }
---
+++ guestbookServlet/src/main/java/guestbook/service/GuestbookWriteServlet.java
@@ -0,0 +1,29 @@
+package guestbook.service;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/GuestbookWriteServlet")
+public class GuestbookWriteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		//데이터
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String homepage = request.getParameter("homepage");
+		String subject = request.getParameter("subject");
+		String content = request.getParameter("content");
+		
+		//DB 연동
+		
+	
+	}
+
+}
