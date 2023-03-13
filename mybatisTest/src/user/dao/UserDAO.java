411cd07115b26ff4d0f88d1b7b3773675bf2e0c4
KIMDONGBEEN <dd1761@naver.com>
2023-03-13 16:54:49 +0900

    23-03-13

--- mybatisTest/src/user/dao/UserDAO.java
+++ mybatisTest/src/user/dao/UserDAO.java
@@ -1,5 +1,12 @@
 package user.dao;
 
+import user.bean.UserDTO;
+
 public class UserDAO {
+
+	public void write(UserDTO userDTO) {
+		
+		
+	}
 
 }
--- mybatisTest/src/user/service/UserInsertService.java
+++ mybatisTest/src/user/service/UserInsertService.java
@@ -2,6 +2,9 @@ package user.service;
 
 import java.util.Scanner;
 
+import user.bean.UserDTO;
+import user.dao.UserDAO;
+
 public class UserInsertService implements UserService {
 
 	@Override
@@ -11,8 +14,25 @@ public class UserInsertService implements UserService {
 		//데이터
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println();
+		System.out.print("이름 입력 : ");
+		String name = scanner.next();
 		
+		System.out.print("id 입력 : ");
+		String id = scanner.next();
+		
+		System.out.print("비밀번호 입력 : ");
+		String pwd = scanner.next();
+		
+		UserDTO userDTO = new UserDTO();
+		userDTO.setName(name);
+		userDTO.setId(id);
+		userDTO.setPwd(pwd);
+		
+		//DB
+		UserDAO userDAO = new UserDAO();
+		userDAO.write(userDTO);
+		
+		System.out.println("데이터를 저장하였습니다.");
 	}
 
 }
